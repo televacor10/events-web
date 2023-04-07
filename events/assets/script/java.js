179a0c5c70b52b9debc46adb4b635ff446429852
televacor10 <121763186+televacor10@users.noreply.github.com>
2023-04-07 08:25:25 -0700

    Add files via upload

---
+++ events/assets/script/java.js
@@ -0,0 +1,10 @@
+
+let sidemenu = document.getElementById('sidemenu');
+
+function closemenu() {
+    sidemenu.style.right = ('-150px');
+
+}
+function openmenu() {
+    sidemenu.style.right = ('0')
+}
